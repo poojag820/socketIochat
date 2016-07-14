35f643cdbf310667eef85cabf2e4ad82c56343d3
abhishek goswami <agoswami@localhost.localdomain>
2016-07-14 18:07:02 +0530

    simple structure for chat app created

---
+++ server.js
@@ -0,0 +1,61 @@
+var http = require('http');
+var fs = require('fs');
+
+//create server
+var server = http.createServer(function(req,res){
+  fs.readFile(__dirname + '/index.html',function(err,data){
+    if(!err){
+      // console.log(data);
+      res.end(data);
+    }
+    else{
+      console.log(err);
+    }
+  });
+
+});
+
+var users = [];
+var userSocketObj  = {};
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection',function(socket){
+  console.log('user connectionc');
+
+  socket.on('add user',function(data){
+    nickname = data.user;
+    var userIndex = users.indexOf(nickname);
+    if(userIndex == -1){
+      callback(true);
+      socket.nickname = nickname;
+      console.log('user'+' ' + socket.nickname + 'connection');
+      users.push(nickname);
+      userSocketObj[nickname] = socket;
+      updateUserInDOM();
+    }
+  });
+
+  socket.on('disconnect',function(){
+    console.log('user disconnect' + socket.username);
+    users.splice(socket.nickname,1);
+    updateUserInDOM();
+  });
+
+  socket.on('new message',function(data){
+    //send message to all
+    io.emit('new message',data);
+  });
+
+});
+
+
+
+function updateUserInDOM(){
+  io.emit('updateUsers',{'users':users});
+}
+
+
+server.listen(8000,function(){
+  console.log('listening on port 8000');
+})
