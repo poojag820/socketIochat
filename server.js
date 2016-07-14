343b4266de8e5efcc962aa8727cd8dc6c401c45a
abhishek goswami <agoswami@localhost.localdomain>
2016-07-14 18:41:30 +0530

    callback created

--- server.js
+++ server.js
@@ -23,21 +23,24 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection',function(socket){
   console.log('user connectionc');
 
-  socket.on('add user',function(data){
+  socket.on('add user',function(data,callback){
     nickname = data.user;
     var userIndex = users.indexOf(nickname);
     if(userIndex == -1){
-      callback(true);
       socket.nickname = nickname;
       console.log('user'+' ' + socket.nickname + 'connection');
       users.push(nickname);
       userSocketObj[nickname] = socket;
+      callback(false);
       updateUserInDOM();
+    }
+    else{
+      callback(true);
     }
   });
 
   socket.on('disconnect',function(){
-    console.log('user disconnect' + socket.username);
+    console.log('user disconnect' + socket.nickname);
     users.splice(socket.nickname,1);
     updateUserInDOM();
   });
